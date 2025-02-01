e3f54a74f87f53a35699dd66c65cc1598b144d7b
umer <157419836+UmrAmr@users.noreply.github.com>
2025-02-02 03:59:19 +0500

    made it yahaoo

---
+++ WebApplication1/WebApplication1/Controllers/loginController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models; //added by umer
+
+namespace WebApplication1.Controllers
+{
+    public class loginController : Controller
+    {
+        DapperHelper db;
+
+        public loginController()
+        {
+            db = new DapperHelper();
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // **GET: Show Registration Page**
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // **POST: Handle Form Submission**
+        [HttpPost]
+        public ActionResult Create(AppUsers user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.InsertUser(user);
+                ViewBag.Message = "Account Created Successfully!";
+            }
+            return View();
+        }
+    }
+}
