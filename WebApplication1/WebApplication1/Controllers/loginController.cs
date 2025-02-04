b3dd600cb11f2f3bc96774b171402b1421067763
umer <157419836+UmrAmr@users.noreply.github.com>
2025-02-04 06:12:11 +0500

    sigin-up-perfrom-login-remained

--- WebApplication1/WebApplication1/Controllers/loginController.cs
+++ WebApplication1/WebApplication1/Controllers/loginController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using WebApplication1.Models; //added by umer
+using Dapper; //added by teacher
+using System.Data; //added by teacher
+using WebApplication1.Models; //added by teacher
+
 
 namespace WebApplication1.Controllers
 {
@@ -22,23 +25,94 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-
-        // **GET: Show Registration Page**
-        public ActionResult Create()
-        {
-            return View();
-        }
-
         // **POST: Handle Form Submission**
         [HttpPost]
-        public ActionResult Create(AppUsers user)
+        public ActionResult Index(AppUsers user)
         {
-            if (ModelState.IsValid)
+            int r = db.Connection.Execute("Insert Into usertable (Username, Password, Name) Values (@Username, @Password, @Name)", user);
+
+            if (r > 0)
             {
-                db.InsertUser(user);
                 ViewBag.Message = "Account Created Successfully!";
             }
+            else
+            {
+                ViewBag.Message = "Error creating account.";
+            }
+
             return View();
         }
+
+        //--------------------------------------------
+        public ActionResult login()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //  public ActionResult login(string username, string password)
+
+        // //public ActionResult login(string username, string password)
+        //{
+        //    //int r = db.Connection.Execute("Insert Into usertable (Username, Password, Name) Values (@Username, @Password, @Name)", user);
+        //  var existingUser = db.Connection.QueryFirstOrDefault<AppUsers>("SELECT * FROM usertable WHERE Username = @Username", new { username = username });
+
+
+        //   if (existingUser != null && existingUser.password == user.password)
+        //  //  if (username == "admin" && password == "admin")
+        //    {
+        //        // User authenticated successfully
+        //        ViewBag.Message = "Login Successful!";
+        //        // You can redirect to another page or store user session info here.
+        //        //return RedirectToAction("login","Index"); // Example: Redirecting to a dashboard.
+        //        return View();            
+        //    }
+
+        //    else
+        //    {
+        //        // Invalid credentials
+        //        ViewBag.Message = "Invalid Username or Password.";
+        //       // return RedirectToAction("Index", "Login");
+        //       return View();
+        //    }
+        //}
+        [HttpPost]
+        public ActionResult login(string username, string password)
+        {
+            try
+            {
+                // Fetch the user from the database
+                var existingUser = db.Connection.QueryFirstOrDefault<AppUsers>(
+                    "SELECT * FROM usertable WHERE Username = @Username",
+                    new { Username = username }
+                );
+
+                // Check if the user exists
+                if (existingUser != null)
+                {
+                    // Compare the password
+                    if (existingUser.password == password)
+                    {
+                        ViewBag.Message = "Login Successful!";
+                        return View(); // Redirect to dashboard if needed
+                    }
+                    else
+                    {
+                        ViewBag.Message = "Invalid Password.";
+                    }
+                }
+                else
+                {
+                    ViewBag.Message = "Invalid Username.";
+                }
+            }
+            catch (Exception ex)
+            {
+                // If an error happens, display it
+                ViewBag.Message = "An error occurred: " + ex.Message;
+            }
+
+            return View();
+        }
+
     }
 }
--- WebApplication1/WebApplication1/Models/DapperHelper.cs
+++ WebApplication1/WebApplication1/Models/DapperHelper.cs
@@ -12,21 +12,21 @@ namespace WebApplication1.Models
 {
     public class DapperHelper
     {
-        public readonly string connection;
+        public readonly SqlConnection Connection;
         public DapperHelper()
         {
-            connection = ConfigurationManager.ConnectionStrings["conn"].ConnectionString.ToString();
+            Connection = new SqlConnection(ConfigurationManager.ConnectionStrings["conn"].ConnectionString.ToString());
         }
 
-        //added this method by umer
-        public void InsertUser(AppUsers user)
-        {
-            using (IDbConnection db = new SqlConnection(connection))
-            {
-                string query = "INSERT INTO Users (username, password, email, name) VALUES (@username, @password, @email, @name)";
-                db.Execute(query, user);
-            }
-        }
+        ////added this method by umer chatgbt
+        //public void InsertUser(AppUsers user)
+        //{
+        //    using (IDbConnection db = new SqlConnection(connection))
+        //    {
+        //        string query = "INSERT INTO Users (username, password, email, name) VALUES (@username, @password, @email, @name)";
+        //        db.Execute(query, user);
+        //    }
+        //}
     }
 }
 
